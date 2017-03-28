10ee68450a3eb63974e628120a032c4d185a4b2a
gagarin55 <gagarin55@protonmail.com>
2017-03-28 16:09:44 +0300

    minor fixes

--- src/router.js
+++ src/router.js
@@ -13,22 +13,7 @@ import { UserIsAuthenticated } from './auth/wrappers.js'
 const history = syncHistoryWithStore(browserHistory, store);
 
 const Authenticated = UserIsAuthenticated((props) => React.cloneElement(props.children, props));
-/*
- Route path="/" component={App}>
- <Route path="login" component={Login}/>
- <Route component={EnsureLoggedInContainer}>
- <Route component={MainLayout}>
- <IndexRoute component={Dashboard}/>
- <Route path="dashboard" component={Dashboard}/>
- <Route path="wallet">
- <IndexRoute component={Wallet}/>
- <Route path="account/:address/send" component={SendWizard} />
- <Route path="account/:address" component={Account} />
- </Route>
- </Route>
- </Route>
- </Route>
- */
+
 export const router = (
     <Provider store={ store }>
         <Router history={ history }>
