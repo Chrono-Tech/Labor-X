e47d54115585da3a876f0c525efa58de90d6cfd8
Andrew Laiff <q.laiff@gmail.com>
2017-06-09 01:03:12 +0500

    Initial implementation

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+const getRoutes = (store, when) => {
+  /* eslint-disable global-require */
+  const modules = [
+    // $$ ADD HERE ~~ Do not remove
+  ]
+
+  const routes = modules
+    .map(module => module(store, when))
+    .filter(Boolean)
+
+  return (
+    <Switch>
+      {routes.map(params => <Route key={params.path} {...params} />)}
+    </Switch>
+  )
+}
+
+export default getRoutes
