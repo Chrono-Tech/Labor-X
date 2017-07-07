596192b2a7532625f8e765ee8c55fe641bcf001f
dvpetrov <dvpetrov.main@gmail.com>
2017-07-07 13:02:39 +0300

    Redux improvements

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-const getRoutes = (store, when) => {
+const getRoutes = context => {
   /* eslint-disable global-require */
   const modules = [
     // ~~ ADD HERE ~~ Do not remove
   ]
 
   const routes = modules
-    .map(module => module(store, when))
+    .map(module => module(context))
     .filter(Boolean)
 
   return (
