3a14c80199d8b0cebbd39e9fb37159c8d70db2d1
Andrew Laiff <q.laiff@gmail.com>
2017-11-06 15:37:06 +0500

    Remove old master

--- src/auth/wrappers.js
+++
@@ -1,9 +0,0 @@
-import { UserAuthWrapper } from 'redux-auth-wrapper'
-import { routerActions } from 'react-router-redux'
-
-export const UserIsAuthenticated = UserAuthWrapper({
-  authSelector: state => state.user,
-  redirectAction: routerActions.replace,
-  failureRedirectPath: 'login', // '/login' by default.
-  wrapperDisplayName: 'UserIsAuthenticated'
-})
--- src/store/store.test.js
+++
@@ -1,7 +0,0 @@
-import { store } from './index'
-
-describe('store', () => {
-  it('should be created', () => {
-    expect(store).toBeDefined()
-  })
-})
