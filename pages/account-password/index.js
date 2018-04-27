48c2ca553f910812e7f4abac67b3e9cc2b98abd9
Sozinov Alexander <sanzeting@mail.ru>
2018-04-28 02:04:10 +0300

    validation forms

---
+++ pages/account-password/index.js
@@ -0,0 +1,35 @@
+import { LoginActions } from 'components/layouts'
+import { LoginForm } from 'components/Login'
+import withRedux from 'next-redux-wrapper'
+import React from 'react'
+import Head from 'next/head'
+import initialStore from 'store'
+import { bootstrap } from 'store/bootstrap'
+import 'styles/globals/globals.scss'
+import ethereumService from '../../src/services/EthereumService'
+import css from './index.scss'
+
+class CreateAccount extends React.Component {
+  static getInitialProps ({ store }) {
+    store.dispatch(bootstrap())
+  }
+
+  componentWillMount () {
+    ethereumService.start()
+  }
+
+  render () {
+    return (
+      <div className={css.root}>
+        <Head>
+          <meta name='viewport' content='initial-scale=1.0, maximum-scale=1.0, user-scalable=no, width=device-width' />
+        </Head>
+        <LoginActions>
+          Create account
+        </LoginActions>
+      </div>
+    )
+  }
+}
+
+export default withRedux(initialStore)(CreateAccount)
