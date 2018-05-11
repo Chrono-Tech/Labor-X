7bc40d58d4adf4d3cb5f9b2dc309c9ffed9c7864
Sozinov Alexander <sanzeting@mail.ru>
2018-05-11 17:01:12 +0300

    back link for select option was added

--- src/components/Login/SelectOption/SelectOption.js
+++ src/components/Login/SelectOption/SelectOption.js
@@ -81,6 +81,12 @@ export default class SelectOption extends React.Component {
               Uport
           </button>
         </div>
+  
+        <div className={css.otherActions}>
+          or
+          <button className={css.backButton} onClick={() => onChangeStep(LoginSteps.SelectWallet)}>back</button>
+        </div>
+        
       </div>
     )
   }
