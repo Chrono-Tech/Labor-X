b7629ab1d3c07b140e4d6d2c7645ee77ea9bf863
maximus <kalashnikov.tema@gmail.com>
2018-06-15 20:08:40 +0300

    Opportunities view

--- src/content/lib/OpportunityViewContent/OpportunityViewContent.js
+++ src/content/lib/OpportunityViewContent/OpportunityViewContent.js
@@ -50,12 +50,15 @@ export class OpportunityViewContent extends React.Component {
           ontop: new BigNumber(0),
         })
       )
-      Router.push('/opportunities')
-    } finally {
-      this.setState({
-        isOfferPosting: false,
-      })
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.warn('handlePostOffer Error: ', e)
     }
+    this.setState({
+      isOfferPosting: false,
+    }, () => {
+      Router.push('/applications-and-offers')
+    })
   }
 
   tabs = [
@@ -105,8 +108,8 @@ export class OpportunityViewContent extends React.Component {
         <div className={css.content}>
           <div className={css.header}>
             <h2>{job.ipfs.name}</h2>
-            <p>Ref {job.ipfs.refString}</p>
-            <p className={css.opportunityAge}>{moment(job.ipfs.period.since).fromNow()}</p>
+            <p>Ref {job.id}</p>
+            <p className={css.opportunityAge}>{moment(job.extra.createdAt).fromNow()}</p>
           </div>
           <div className={css.tabs}>
             {this.tabs.map((tab, index) => (
