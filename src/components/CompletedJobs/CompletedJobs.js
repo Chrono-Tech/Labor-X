3915d83a7984fa0705143ded3e84a8c0dad224c9
Alan Khanaev <info@stampit.world>
2018-07-05 14:31:40 +0400

    Implement completed jobs, markup and binding

--- src/components/CompletedJobs/CompletedJobs.js
+++ src/components/CompletedJobs/CompletedJobs.js
@@ -1,13 +1,24 @@
-import { Translate } from 'components/common'
-import { CompletedJobCard } from 'components/common'
-import { connect } from 'react-redux'
-import { signerSelector, countFinishedJobsByWorkerSelector, finishedJobsByWorkerSelector, finalizedJobsByWorkerSelector, pendingFinishJobsByWorkerSelector } from 'src/store'
+import PropTypes from 'prop-types'
 import React from 'react'
+import { connect } from 'react-redux'
+import { JobModel } from 'src/models'
+import { Translate, CompletedJobCard } from 'components/common'
+import { signerSelector, monthWorkerEraningSelector, totalWorkerEraningSelector, countFinishedJobsByWorkerSelector, finishedJobsByWorkerSelector, finalizedJobsByWorkerSelector, pendingFinishJobsByWorkerSelector } from 'src/store'
 import css from './CompletedJobs.scss'
 
+const courseUsdLhus = 18
+
 class CompletedJobs extends React.Component {
-  render() {
-    const { finishedCount, finishedJobs, finalizedJobs, pendingFinishJobs } = this.props;
+  static propTypes = {
+    finishedCount: PropTypes.number,
+    finishedJobs: PropTypes.arrayOf(PropTypes.instanceOf(JobModel)),
+    finalizedJobs: PropTypes.arrayOf(PropTypes.instanceOf(JobModel)),
+    pendingFinishJobs: PropTypes.arrayOf(PropTypes.instanceOf(JobModel)),
+    totalEarn: PropTypes.number,
+    monthEarn: PropTypes.number,
+  }
+  render () {
+    const { finishedCount, finishedJobs, finalizedJobs, pendingFinishJobs, totalEarn, monthEarn } = this.props
     return (
       <div className={css.main}>
         <div className={css.title}>
@@ -26,44 +37,55 @@ class CompletedJobs extends React.Component {
             <div className={css.blockTotalEarned}>
               <div className={css.blockTotalEarnedLx}>
                 <span className={css.lxIcon}>lh-light</span>
-                <span className={css.earnedTotalLx}>28.85</span>
+                <span className={css.earnedTotalLx}>{totalEarn}</span>
               </div>
               <div className={css.blockTotalEarnedUsd}>
-                Earned Total ($1,154.20)
+                Earned Total (${courseUsdLhus * totalEarn})
               </div>
             </div>
 
             <div className={css.blockMonthEarned}>
               <div className={css.blockTotalEarnedLx}>
                 <span className={css.lxIcon}>lh-light</span>
-                <span className={css.earnedMonthLx}>15.00</span>
+                <span className={css.earnedMonthLx}>{monthEarn}</span>
               </div>
               <div className={css.blockTotalEarnedUsd}>
-                Earned this month ($450.00)
+                Earned this month (${courseUsdLhus * monthEarn})
               </div>
             </div>
 
           </div>
         </div>
         <div className={css.content}>
-          <div className={css.invoiceSentTitle}>
-            Invoice Sent
-          </div>
-
+          {
+            pendingFinishJobs.length > 0 && (
+              <div className={css.invoiceSentTitle}>
+                Invoice Sent
+              </div>
+            )
+          }
           {
             pendingFinishJobs.map((job) => <CompletedJobCard key={job.id} job={job} />)
           }
 
-          <div className={css.completedTitle}>
-            Completed
-          </div>
+          {
+            finishedJobs.length > 0 && (
+              <div className={css.completedTitle}>
+                Completed
+              </div>
+            )
+          }
           {
             finishedJobs.map((job) => <CompletedJobCard key={job.id} job={job} />)
           }
 
-          <div className={css.finalizedTitle}>
-            Finalized
-          </div>
+          {
+            finalizedJobs.length > 0 && (
+              <div className={css.finalizedTitle}>
+                Finalized
+              </div>
+            )
+          }
           {
             finalizedJobs.map((job) => <CompletedJobCard key={job.id} job={job} />)
           }
@@ -73,26 +95,29 @@ class CompletedJobs extends React.Component {
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   const signer = signerSelector()(state)
   const finishedCount = countFinishedJobsByWorkerSelector(signer.address)(state)
-  const finishedJobs = finishedJobsByWorkerSelector(signer.address)(state);
-  const finalizedJobs = finalizedJobsByWorkerSelector(signer.address)(state);
-  const pendingFinishJobs = pendingFinishJobsByWorkerSelector(signer.address)(state);
+  const finishedJobs = finishedJobsByWorkerSelector(signer.address)(state)
+  const finalizedJobs = finalizedJobsByWorkerSelector(signer.address)(state)
+  const pendingFinishJobs = pendingFinishJobsByWorkerSelector(signer.address)(state)
+  const totalEarn = totalWorkerEraningSelector(signer.address)(state)
+  const monthEarn = monthWorkerEraningSelector(signer.address)(state)
   return {
     signer,
     finishedCount,
     finishedJobs,
     pendingFinishJobs,
-    finalizedJobs
+    finalizedJobs,
+    totalEarn,
+    monthEarn,
   }
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps () {
   return {
     onPostOffer: () => { },
   }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(CompletedJobs);
+export default connect(mapStateToProps, mapDispatchToProps)(CompletedJobs)
--- src/components/common/CompletedJobCard/CompletedJobCard.js
+++ src/components/common/CompletedJobCard/CompletedJobCard.js
@@ -1,68 +1,125 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import { JobModel } from 'src/models'
+import t from "typy"
+import moment from 'moment'
+import { JOB_STATE_PENDING_FINISH, JOB_STATE_FINISHED, JOB_STATE_FINALIZED, JobModel } from 'src/models'
 import { Link } from 'src/components/common'
-import { JOB_STATE_PENDING_FINISH, JOB_STATE_FINISHED, JOB_STATE_FINALIZED } from 'src/models'
 import css from './CompletedJobCard.scss'
 
 const dateFormat = 'DD MMM YYYY'
+const courseUsdLhus = 18
 
 export default class CompletedJobCard extends React.Component {
   static propTypes = {
     job: PropTypes.instanceOf(JobModel).isRequired,
   }
 
-  constructor(...args) {
+  constructor (...args) {
     super(...args)
   }
 
-  handleClickReview = () => {
-    this.props.onClickReviewOffer(this.props.job.id)
+  getWorkedHourse = (job) => {
+    const jobStateIndex = t(job, "state.index").safeObject
+    const startTime = moment(t(job, "extra.startTime").safeObject)
+    if (jobStateIndex === JOB_STATE_FINISHED.index) {
+      const endTime = moment(t(job, "extra.finishTime").safeObject)
+      const duration = moment.duration(endTime.diff(startTime))
+      return duration.asHours().toFixed(1) + "h"
+    }
+    if (jobStateIndex === JOB_STATE_FINALIZED.index) {
+      const endTime = moment(t(job, "extra.finalizedAt").safeObject)
+      const duration = moment.duration(endTime.diff(startTime))
+      return duration.asHours().toFixed(1) + "h"
+    }
+    if (jobStateIndex === JOB_STATE_PENDING_FINISH.index) {
+      const endTime = moment(t(job, "extra.pendingFinishAt").safeObject)
+      const duration = moment.duration(endTime.diff(startTime))
+      return duration.asHours().toFixed(1) + "h"
+    }
+    return " "
+  }
+
+  getTotalHours = (job) => {
+    const fixedPrice = t(job, "ipfs.budget.fixedPrice").safeObject
+    const totalHours = t(job, "ipfs.budget.totalHours").safeObject
+    if (fixedPrice)
+    {return " "}
+    else
+    {return `${totalHours}h`}
+  }
+
+  getJobLhus = (job) => {
+    const fixedPrice = t(job, "ipfs.budget.fixedPrice").safeObject
+    if (fixedPrice)
+    {return fixedPrice}
+    else {
+      const hourlyRate = Number(t(job, "ipfs.budget.hourlyRate").safeObject)
+      const totalHours = Number(t(job, "ipfs.budget.totalHours").safeObject)
+      return hourlyRate * totalHours
+    }
   }
 
   renderStateLabel = (jobState) => {
     if (!jobState)
-      return null;
+    {return null}
     if (jobState.index === JOB_STATE_FINISHED.index)
-      return <div className={css.completedLabel}>COMPLETED</div>
+    {return <div className={css.completedLabel}>COMPLETED</div>}
     if (jobState.index === JOB_STATE_FINALIZED.index)
-      return <div className={css.finalizedLabel}>FINALIZED</div>
+    {return <div className={css.finalizedLabel}>FINALIZED</div>}
     if (jobState.index === JOB_STATE_PENDING_FINISH.index)
-      return <div className={css.invoiceSentLabel}>INVOICE SENT</div>
+    {return <div className={css.invoiceSentLabel}>INVOICE SENT</div>}
   }
 
-  renderFooter = () => {
-    const { job } = this.props
+  renderFooter = (job) => {
     return (
       <div className={css.cardFooter}>
         {this.renderStateLabel(job.state)}
-        <img  width="24" height="24" alt="" title="Message"  src="/static/images/svg/message-blue.svg" />
+        <img width='24' height='24' alt='' title='Message' src='/static/images/svg/message-blue.svg' />
       </div>
     )
   }
 
-  render() {
+  renderJobDate = (job) => {
+    if (job.state.index === JOB_STATE_FINISHED.index)
+    {return moment(t(job, "extra.finishTime").safeObject).format(dateFormat)}
+    if (job.state.index === JOB_STATE_FINALIZED.index)
+    {return moment(t(job, "extra.finalizedAt").safeObject).format(dateFormat)}
+    if (job.state.index === JOB_STATE_PENDING_FINISH.index)
+    {return moment(t(job, "extra.pendingFinishAt").safeObject).format(dateFormat)}
+    return "Not found"
+  }
+
+  renderHours = (job) => {
+    const fixedPrice = t(job, "ipfs.budget.fixedPrice").safeObject
+    if (fixedPrice)
+    {return " "}
+    else
+    {return this.getWorkedHourse(job) + " / " + this.renderTotalHours(job)}
+  }
+
+  render () {
     const { job } = this.props
+    const workCostLhus = this.getJobLhus(job)
+    const workCostUsd = workCostLhus * courseUsdLhus
     return (
       <div
         className={css.root}
       >
         <div className={css.cardInfo}>
-          <Link className={css.cardName} href={`/client-job-view/${job.id}`}>
-            <h4>job.ipfs.name</h4>
+          <Link className={css.cardDate} href={`/client-job-view/${job.id}`}>
+            <h4> {this.renderJobDate(job)}  </h4>
           </Link>
-          <div className={css.cardDate}>
-          Install 10 Gas Ovens
+          <div className={css.cardName}>
+            {t(job, "ipfs.name").safeObject}
           </div>
           <div className={css.cardWorkingHourse}>
-          38h/ 40h
+            {this.renderHours(job)}
           </div>
           <div className={css.cardCostPayment}>
-          LHUS 76.25 ($3,050.00)
+            LHUS {workCostLhus} (${workCostUsd})
           </div>
         </div>
-        {this.renderFooter()}
+        {this.renderFooter(job)}
       </div>
     )
   }
