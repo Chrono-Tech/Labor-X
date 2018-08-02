0b3ad5ffa83c63bafba8a938d76cba032af8d768
Alan Khanaev <info@stampit.world>
2018-08-02 09:18:27 +0400

    LABX-376 implement getInitialProps action page loader

--- src/index.js
+++ src/index.js
@@ -83,6 +83,7 @@ import {
   activeJobs,
   profiles,
   reviewApplicants,
+  completedJobs,
 } from "src/store/reducers"
 
 import 'styles/globals/globals.scss'
@@ -130,6 +131,7 @@ const reducer = combineReducers({
   clientProfile,
   activeJobs,
   reviewApplicants,
+  completedJobs,
 })
 
 const store = createStore(
